93d01fba3cf318de016b14d5794eacfb19a1eea7
Anaru03 <dele22428@uvg.edu.gt>
2024-02-28 16:47:08 -0600

    Modifications in the docker file

--- database/DataBase.js
+++ database/DataBase.js
@@ -1,1 +1,6 @@
 import conn from './conn.js'
+
+export async function createPost(title, content) {
+    const [result] = await db.query('INSERT INTO blog_posts (title, content) VALUES (?, ?)', [title, content])
+    return result
+ }
--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 //Hacer los endpoints
+
+/*
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://127.0.0.1:${port}`)
+})
+*/
+
+import express from 'express'
+import { getAllPosts } from './database/DataBase.js'
+
+const app = express()
+app.use(express.json())
+
+app.get('/posts', async (req, res) => {
+    const posts = await getAllPosts()
+    res.json(posts)
+})
+
+//... resto de su codigo ...
