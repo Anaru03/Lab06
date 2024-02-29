8b5a9a340edc41f7eb54ed0d6348e5bb76178066
Anaru03 <dele22428@uvg.edu.gt>
2024-02-29 09:34:28 -0600

    Successful bug implementation

--- main.js
+++ main.js
@@ -1,72 +1,124 @@
-//main.js
-
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { db } from './database/DataBase.js';
 import { getAllPosts, getPostById, createPost, updatePost, deletePost } from './database/DataBase.js';
+import cors from 'cors';
+
 
 const app = express();
+const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 
-app.get('/posts', async (req, res) => {
-    const posts = await getAllPosts();
-    res.json(posts);
-});
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Blog API',
+        version: '1.0.0',
+        description: 'API for managing blog posts',
+      },
+      servers: [{ url: 'http://localhost:3000' }],
+    },
+    apis: ['main.js'],
+  };
 
-app.get('/posts/:postId', async (req, res) => {
-    const postId = req.params.postId;
-    const post = await getPostById(postId);
-    if (post) {
-        res.json(post);
-    } else {
-        res.status(404).json({ error: 'Post not found' });
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec)
+);
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error interno del servidor');
+  });
+
+app.get('/posts', async (req, res, next) => {
+    try {
+        const posts = await getAllPosts();
+        res.json(posts);
+    } catch (error) {
+        next(error); 
     }
 });
 
-app.post('/posts', async (req, res) => {
-    const newPost = req.body;
-    const createPost = await createPost(newPost);
-    res.json(createPost);
-});
-
-app.put('/posts/:postId', async (req, res) => {
-    const postId = req.params.postId;
-    const updatedPostData = req.body;
-    const updatedPost = await updatePost(postId, updatedPostData);
-    if (updatedPost) {
-        res.json(updatedPost);
-    } else {
-        res.status(404).json({ error: 'Post not found' });
+app.get('/posts/:postId', async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        const post = await getPostById(postId);
+        if (post) {
+            res.json(post);
+        } else {
+            res.status(404).json({ error: 'Error: Post not found' });
+        }
+    } catch (error) {
+        next(error);
     }
 });
 
-app.delete('/posts/:postId', async (req, res) => {
-    const postId = req.params.postId;
-    const deletedPost = await deletePost(postId);
-    if (deletedPost) {
-        res.json({ message: 'Post deleted successfully' });
-    } else {
-        res.status(404).json({ error: 'Post not found' });
+app.post('/posts', async (req, res, next) => {
+    try {
+        const newPost = req.body;
+        const createPost = await createPost(newPost);
+        res.status(200).json(createPost);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.put('/posts/:postId', async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        const updatedPostData = req.body;
+        const updatedPost = await updatePost(postId, updatedPostData);
+        if (updatedPost) {
+            res.json(updatedPost);
+        } else {
+            res.status(404).json({ error: 'Post not found' });
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.delete('/posts/:postId', async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        const deletedPost = await deletePost(postId);
+        if (deletedPost) {
+            res.json({ message: 'Post deleted successfully' });
+        } else {
+            res.status(404).json({ error: 'Post not found' });
+        }
+    } catch (error) {
+        next(error);
     }
 });
 
 app.get('/', (req, res) => {
-  res.send('Ruth de León');
-}); //Tengo que transformar esto para subir archivos estáticos.
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server listening at http://127.0.0.1:${port}`);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+  
+app.get('/html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'html.html'));
+});
+  
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'css', 'styles.css'));
+});
+  
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'js', 'script.js'));
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://127.0.0.1:${PORT}/`);
+});
