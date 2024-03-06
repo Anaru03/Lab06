a6cfba3e93dae1d41e141e1932294d8b64554889
Anaru03 <dele22428@uvg.edu.gt>
2024-03-05 20:38:55 -0600

    Change querys table

--- database/DataBase.js
+++ database/DataBase.js
@@ -12,32 +12,32 @@ export const db = {
 };
 
 export async function getPosts() {
-  const [rows] = await conn.query('select * from post_content');
+  const [rows] = await conn.query('select * from Post_content');
   return rows
 }
 
 export async function createPost(title, content, author_name, images_content) {
-  const [result] = await conn.query('insert into post_content (title, content, author_name, images_content) values (?, ?, ?, ?)', [title, content, author_name])
+  const [result] = await conn.query('insert into Post_content (title, content, author_name, images_content) values (?, ?, ?, ?)', [title, content, author_name])
   return result
 }
 
 export async function getPostId(id) {
-  const [rows] = await conn.query('select * from post_content where id = ?', [id]);
+  const [rows] = await conn.query('select * from Post_content where id = ?', [id]);
   return rows
 }
 
 export async function updatePost(id, title, content, author_name, images_content) {
-  const [result] = await conn.query('update post_content set title = ?, content = ?, author_name = ?, images_content = ? where id = ?', [title, content, author_name, images_content, id])
+  const [result] = await conn.query('update Post_content set title = ?, content = ?, author_name = ?, images_content = ? where id = ?', [title, content, author_name, images_content, id])
   return result  
 }
 
 export async function deletePost(id) {
-  const [result] = await conn.query('delete from post_content where id = ?', [id])
+  const [result] = await conn.query('delete from Post_content where id = ?', [id])
   return result
 }
 
 export async function insertImage(images_content) {
-  const [result] = await conn.query('INSERT INTO post_content (images_content) VALUES (?)', [images_content]);
+  const [result] = await conn.query('INSERT INTO Post_content (images_content) VALUES (?)', [images_content]);
   return result;
 }
 
