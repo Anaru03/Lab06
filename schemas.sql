346139f4cf19ec792b80eaa99e9661bb373e8dd7
Anaru03 <dele22428@uvg.edu.gt>
2024-03-06 19:22:53 -0600

    Fix schemas.sql

--- schemas.sql
+++ schemas.sql
@@ -1,8 +1,8 @@
-create table IF not exists Post_Content ( 
-    id int AUTO_INCREMENT primary key,
-    title varchar(225) not null,
-    content text not null,
-    created_at timestamp default CURRENT_TIMESTAMP
-    images_content varchar(225)
-    author_name varchar(225) not null
+CREATE TABLE IF NOT EXISTS Post_Content ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(225) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    images_content VARCHAR(225),
+    author_name VARCHAR(225) NOT NULL
 );
