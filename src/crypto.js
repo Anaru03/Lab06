fcc0029b4ca88d34f9ecc995bea1050a00436d2f
Anaru03 <dele22428@uvg.edu.gt>
2024-04-29 13:16:12 -0600

    Implement JWT

---
+++ src/crypto.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+export const hashPassword = async (password) => {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    return bcrypt.hashSync(password, salt);
+}
+export const comparePassword = (password, hashcontra) => bcrypt.compareSync(password, hashcontra);
